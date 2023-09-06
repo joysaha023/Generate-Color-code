5135c4548383636c285244589b613ea1f2eeb71a
joysaha <joysahacse23@gmail.com>
2023-09-06 09:59:59 +0600

    generate code

---
+++ app.js
@@ -0,0 +1,28 @@
+// Steps
+
+// Step 1 - create onload handler
+window.onload = () => {
+    main();
+
+};
+
+function main() {
+    const root = document.getElementById('root');
+    const btn = document.getElementById('change-btn');
+    const output = document.getElementById('output');
+
+    btn.addEventListener('click', function(){
+        const bgColor = generateHexColor();
+        root.style.backgroundColor = bgColor;
+        output.value = bgColor;
+    })
+}
+
+//step 2 random color generator
+function generateHexColor(){
+        const red = Math.floor(Math.random() * 255);
+        const green = Math.floor(Math.random() * 255);
+        const blue = Math.floor(Math.random() * 255);
+
+        return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
